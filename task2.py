5b06fe0998178561b284059698154ad5459cde58
noth1ngt0say <born.todie702@gmail.com>
2024-10-01 16:53:38 +0300

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,29 @@
+# todo: Преобразуйте переменную age и foo в число
+# age = "23"
+# foo = "23abc"
+#
+# Преобразуйте переменную age в Boolean
+# age = "123abc"
+#
+# Преобразуйте переменную flag в Boolean
+# flag = 1
+#
+# Преобразуйте значение в Boolean
+# str_one = "Privet"
+# str_two = ""
+#
+# Преобразуйте значение 0 и 1 в Boolean
+#
+# Преобразуйте False в строку
+
+age = int(age)
+foo = int(foo)                  # Будет ошибка
+age_to_bool = bool("123abc")
+flag = bool(1)
+str_one = bool("Privet")
+str_two = bool("")
+zero = bool(0)
+one = bool(1)
+st = str(False)
+
+
